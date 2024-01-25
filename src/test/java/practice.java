99e619062caef2ff0688c8e766ac6ac146ee1adc
9876 <9876@L-INE-1YL4PL3.ardianet.net>
2024-01-25 10:18:31 +0530

    for practice

---
+++ src/test/java/practice.java
@@ -0,0 +1,21 @@
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+
+		int a = 12;
+		int b = 15;
+
+		System.out.println(a+b);
+
+		int c = 23, d = 25; 
+
+		System.out.println("c>d");
+
+			}
+	}
+
+
+
